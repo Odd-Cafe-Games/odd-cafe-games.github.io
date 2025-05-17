945c73b41d2145761819265ac99c764f66607d9f
htran0109 <htran0109@gmail.com>
2025-05-17 04:52:31 -0700

    Working banner animation, updating themes for site to work accordingly

--- src/components/fullScreenBanner.tsx
+++ src/components/fullScreenBanner.tsx
@@ -1,18 +1,42 @@
 import Header from "./header"
+import {useState} from "react"
+import "../styles/mask.css"
 
 const FullScreenBanner = () => {
+
+  const [isNight, setIsNight] = useState(true);
+  const [animating, setAnimating] = useState(false)
+
+  const toggleNight = () => {
+    setIsNight(!isNight)
+  };
+
+  const animateDayTransition = () => {
+    if (animating) return;
+    toggleNight()
+    setAnimating(true);
+    setTimeout(() => setAnimating(false), 1500); // match animation time
+  };
+
   return (
-    <div
-      style={{
-        backgroundImage: `url('/assets/banner.png')`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '100vh',
-        width: '100%', 
-      }}
-    >
-      <Header/>
-    </div>
+
+      <div className="relative w-screen h-screen overflow-hidden mb-6">
+        {/* Background layer displaying the new image */}
+        <div
+              className= "absolute w-full h-full bg-cover bg-center"
+              style={{ backgroundImage: isNight ? `url('/assets/banner.png')`: `url('assets/bannerday.png')`, zIndex: 1}}
+        >
+          <Header onToggleFunc={animateDayTransition}/>
+        </div>
+
+        <div
+              className={`absolute w-screen h-screen bg-cover bg-center mask-fade ${
+                animating ? 'animate' : 'hidden'
+              }`}
+              style={{ backgroundImage: isNight ? `url('/assets/bannerday.png')`: `url('assets/banner.png')`, zIndex: 2}}
+        >
+        </div>
+      </div>
   );
 };
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-const Header: React.FC = () => {
+type HeaderProps = {
+  onToggleFunc: () => void;
+};
+
+const Header: React.FC<HeaderProps> = ({onToggleFunc}) => {
   return (
-    <header className="flex items-center justify-between px-6 py-4 bg-transparent text-white">
+    <header className="flex items-center justify-between px-6 py-4 bg-transparent text-white" style={{zIndex: 10}}>
       <nav className="flex space-x-4">
-        <div className="px-4 py-2 rounded-md transition">
+        <a className="px-4 py-2 rounded-md hover:bg-neutral-900 transition" onClick={onToggleFunc}>
           Odd Cafe Games
-        </div>
-        <a href="#Projects" className="px-4 py-2 hover:bg-neutral-900 transition">
+        </a>
+        <a href="#Projects" className="px-4 py-2 rounded-md hover:bg-neutral-900 transition">
           Projects
         </a>
       </nav>
