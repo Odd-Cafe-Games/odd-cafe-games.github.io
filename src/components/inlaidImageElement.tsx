618adfafb0f064dc606920d8c8bad2943e50a249
htran0109 <htran0109@gmail.com>
2025-06-13 18:39:42 -0700

    Website V2

---
+++ src/components/inlaidImageElement.tsx
@@ -0,0 +1,26 @@
+import { PropsWithChildren } from "react";
+import { ReactNode } from "react";
+
+type InlaidImageProps = {
+    imageRef: string;
+    reverse: boolean;
+} & { children?: ReactNode };
+
+const InlaidImage: React.FC<InlaidImageProps> = ({imageRef, reverse, children}) => {
+    return <>
+        <div className={`relative min-h-[70vh] flex flex-col md:flex-row items-center justify-center p-4 w-full ${reverse ? 'md:flex-row-reverse': ''}`}>
+            
+            <div className={`${reverse ? '-mr-[20%]': '-ml-[20%]'} md:top-[25%] h-[80%] md:h-[80%] md:w-[45%] aspect-[17/13]`}>
+                <img src={imageRef} className={`w-full h-full ${reverse ? 'rounded-l-4xl': 'rounded-r-4xl'}`} />
+            </div>
+            <div className="w-[5%]">
+
+            </div>
+            <div className="text-center md:text-left y-10 md:px-15 pt-15 md:top-[50%] w-[100%] md:w-[50%]">
+                {children}
+            </div>
+        </div>
+    </>
+}
+
+export default InlaidImage
--- src/components/projectsSection.tsx
+++ src/components/projectsSection.tsx
@@ -1,96 +1,9 @@
 import { useState, useEffect } from "react";
-
-const images = [
-  "/assets/kittyMaker/kittyHome.png",
-  "/assets/kittyMaker/calendar.png",
-  "/assets/kittyMaker/merlinVN.png",
-  "/assets/kittyMaker/sistersVN.png",
-];
+import ImageCarousel from "./imageCarousel"
+import InlaidImage from "./inlaidImageElement"
 
 type ProjectsProps = {
   isNight: boolean;
-};
-
-const ImageCarousel: React.FC = () => {
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
-  return <>
-  <div className="relative p-4 md:mb-0 w-full max-w-[1200px] aspect-[16/9]">
-    <div className="relative w-full h-full overflow-visible">
-      {images.map((src, index) => {
-        const total = images.length;
-        const relativeIndex = (index - currentIndex + total) % total;
-
-        let transform = "";
-        let zIndex = 10;
-        let opacity = "opacity-0";
-
-        if (relativeIndex === 0) {
-          transform = "translate-x-0 scale-100 rotate-0";
-          zIndex = 30;
-          opacity = "opacity-100";
-        } else if (relativeIndex === 1) {
-          transform = "translate-x-[10%] scale-[0.9] rotate-6";
-          zIndex = 20;
-          opacity = "opacity-30";
-        } else if (relativeIndex === total - 1) {
-          transform = "-translate-x-[10%] scale-[0.9] -rotate-4";
-          zIndex = 20;
-          opacity = "opacity-30";
-        }
-
-        return (
-        <div
-          key={index}
-          className={`
-            absolute top-0 left-0 right-0 mx-auto w-[80%] h-full flex items-center justify-center
-            transition-all duration-500 ease-in-out
-            ${transform} ${opacity}
-          `}
-          style={{ zIndex }}
-        >
-          <div className="bg-white p-4 shadow-md w-full h-full flex items-center justify-center">
-            <img
-              src={src}
-              alt={`Slide ${index + 1}`}
-              className="object-cover w-full h-full"
-            />
-          </div>
-        </div>
-        );
-      })}
-
-      {/* Arrows */}
-      <button
-        onClick={handlePrev}
-        className="absolute left-0 top-1/2 -translate-y-1/2 text-white text-2xl md:text-8xl py-1 rounded z-50"
-        aria-label="Previous"
-      >
-        &lt;
-      </button>
-      <button
-        onClick={handleNext}
-        className="absolute right-0 top-1/2 -translate-y-1/2 text-white text-2xl md:text-8xl py-1 rounded z-50"
-        aria-label="Next"
-      >
-        &gt;
-      </button>
-    </div>
-  </div>
-  </>
 };
 
 const ProjectsSection: React.FC<ProjectsProps> = ({isNight}) => {
@@ -109,12 +22,30 @@ const ProjectsSection: React.FC<ProjectsProps> = ({isNight}) => {
           and are in charge of raising her.
           <br />
         </p>
-        <p>
-          <br />
-          Teach the small cat how to work, play, and build relationships with
-          other cats to uncover more about her mysterious past.
-        </p>
       </div>
+      <InlaidImage imageRef="/assets/kittyMaker/rest_image.png" reverse={false}>
+        <div className="flex-col items-justify items-center">
+          <h1 className={`text-4xl md:text-6xl font-playfair font-semibold`}>
+            The Kitty Digest
+          </h1>
+          <p className="text-lg md:text-2xl font-inter font-medium">
+            <br />
+            Teach the small cat how to work, play, and build relationships with
+            other cats to uncover more about her mysterious past.
+          </p>
+        </div>
+      </InlaidImage>
+      <InlaidImage imageRef="/assets/kittyMaker/cafe_image.png" reverse={true}>
+        <div className="flex-col items-justify items-center">
+          <h1 className={`text-4xl md:text-6xl font-playfair font-semibold`}>
+            The Kitty Digest
+          </h1>
+          <p className="text-lg md:text-2xl font-inter font-medium">
+            <br />
+            Plan a schedule and level up skills to become the greatest cat around! Meet new friends and help them grow together.
+          </p>
+        </div>
+      </InlaidImage>
     </section>
   );
 };
